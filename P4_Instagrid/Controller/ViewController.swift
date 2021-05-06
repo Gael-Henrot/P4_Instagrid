00004b6d784d743c6eb0ea18154e1eae073d7d05
Gael-Henrot <gael.henrot@expleogroup.com>
2021-05-06 17:09:21 +0200

    Use of stack view in pattern view Use of State Config in Selector stack view

--- P4_Instagrid/Controller/ViewController.swift
+++ P4_Instagrid/Controller/ViewController.swift
@@ -4,16 +4,45 @@
 //
 //  Created by Gaël HENROT on 03/05/2021.
 //
+/*
+ L'application permet de créer des petits montages carrés de 3 ou 4 photods.
+ 1. L'utilisateur choisit une des trois dispositions
+    1.1 L'utlisateur appuie sur un des trois boutons:
+        a. La coche de l'ancienne sélection est cachée
+        b. La coche de la nouvelle sélection est affichée.
+        c. La grille centrale change pour correspondre au choix de l'utilisateur
+ 2. L'utilisateur choisit 3 ou 4 photos
+    2.1 L'utilisateur appuie sur un plus
+    2.2 L'utilisateur choisit une photo
+ 3. L'utilisateur partage sa création
+    3.1 L'utilisateur glisse la grille vers le haut ou la gauche
+    3.2 La vue de partage apparait
+    3.3 L'utilisateur choisit son moyen de partage
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    @IBOutlet weak var firstLayoutButton: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        firstLayoutButton?.setTitle("toto", for: .normal)
+        
     }
-
+    
+   /* @IBAction func imageButtonSelected(_ sender: UIButton) {
+        if sender == firstLayoutButton {
+            // meme bouton
+        } else {
+            // Pas meme bouton
+        }
+    }
+    
+    private func buttonSelected(buttonSelected: UIButton) {
+        
+    }*/
 
 }
-
