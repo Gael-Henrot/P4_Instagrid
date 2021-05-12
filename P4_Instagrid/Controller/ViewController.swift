143f0e5a0d35e7ae0a603bb7e50363acdc65c8dd
Gael-Henrot <gael.henrot@expleogroup.com>
2021-05-12 10:09:43 +0200

    allows user to choose a photo by tapping the plus button (the photo is put in the square before the user has chosen the photo) the grid can be slide out of the screen in portrait orientation

--- P4_Instagrid/Controller/ViewController.swift
+++ P4_Instagrid/Controller/ViewController.swift
@@ -38,12 +38,21 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var upRightButton: UIButton?
     @IBOutlet weak var downLeftButton: UIButton?
     @IBOutlet weak var downRightButton: UIButton?
+    
+//    Declaration of the grid
+    @IBOutlet weak var grid: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         changePattern(pattern: .pattern21) // Define the default view
         
-//        firstLayoutButton?.setTitle("toto", for: .normal)
+        let shareGesture = UIPanGestureRecognizer(target: self, action: #selector(dragTheGrid(_:)))
+        grid.addGestureRecognizer(shareGesture)
+
+//        let shareGesture = UISwipeGestureRecognizer(target: self, action: #selector(dragTheGrid(_:)))
+//                Think about changing it according to orientation
+//                shareGesture.direction = .up
+//        grid.addGestureRecognizer(shareGesture)
         
     }
     
@@ -51,13 +60,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     //MARK: Pattern selector
     //=======================================
     
-   /* @IBAction func imageButtonSelected(_ sender: UIButton) {
-        if sender == firstLayoutButton {
-            // meme bouton
-        } else {
-            // Pas meme bouton
-        }
-    }*/
     enum PatternType {
         case pattern12, pattern21, pattern22
     }
@@ -104,38 +106,127 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     //MARK: Choose a photo
     //=======================================
     
-    @IBAction func upLeftChooseAPhoto(_ sender: Any) {
-        chooseAPhoto()
-    }
+    var imageChoosed : UIImage?
+    /*enum LocationType {
+        case upLeft, upRight, downLeft, downRight
+    }*/
     
     func chooseAPhoto() {
+        
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
         
         let chooseAPhotoActionSheet = UIAlertController(title: "Photo Source", message: "Choose a source of photo", preferredStyle: .actionSheet)
         
         chooseAPhotoActionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
-        imagePickerController.sourceType = .camera
-        self.present(imagePickerController, animated: true, completion: nil)
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePickerController.sourceType = .camera
+            self.present(imagePickerController, animated: true, completion: nil)
+            } else {
+                print("Camera not available")
+            }
         }))
         
         chooseAPhotoActionSheet.addAction(UIAlertAction(title: "Library", style: .default, handler: { (action:UIAlertAction) in
-        imagePickerController.sourceType = .photoLibrary
+            imagePickerController.sourceType = .photoLibrary
         self.present(imagePickerController, animated: true, completion: nil)
         }))
         
         chooseAPhotoActionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         
         self.present(chooseAPhotoActionSheet, animated: true, completion: nil)
+        
+        /*func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+    //        upLeftButton?.setImage(image, for: .normal)
+            switch locationType {
+            case LocationType.upLeft:
+                upLeftButton?.setImage(image, for: .normal)
+            case LocationType.upRight:
+                upRightButton?.setImage(image, for: .normal)
+            case LocationType.downLeft:
+                downLeftButton?.setImage(image, for: .normal)
+            case LocationType.downRight:
+                downRightButton?.setImage(image, for: .normal)
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true, completion: nil)
+        }*/
+        
+        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        upLeftButton?.setImage(image, for: .normal)
+//        upLeftButton?.setImage(image, for: .normal)
+        imageChoosed = image
         picker.dismiss(animated: true, completion: nil)
     }
-    /*func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
-    }*/
+    }
+    
+    //Declaration of plus buttons actions
+    @IBAction func upLeftChooseAPhoto(_ sender: UIButton) {
+        chooseAPhoto()
+//        sender.backgroundImage(for: .normal) = imageChoosed
+        sender.setImage(imageChoosed, for: .normal)
+    }
+
+    @IBAction func upRightChooseAPhoto(_ sender: UIButton) {
+        chooseAPhoto()
+        sender.setImage(imageChoosed, for: .normal)
+    }
+    
+    @IBAction func downLeftChooseAPhoto(_ sender: UIButton) {
+        chooseAPhoto()
+        sender.setImage(imageChoosed, for: .normal)
+    }
+    
+    @IBAction func downRightChooseAPhoto(_ sender: UIButton) {
+        chooseAPhoto()
+        sender.setImage(imageChoosed, for: .normal)
+    }
+    
+    
+    //=======================================
+    //MARK: Share the grid
+    //=======================================
+    
+    @objc func dragTheGrid(_ sender: UIPanGestureRecognizer) {
+        // think to add a condition (only use .up direction if the device is portrait and .left if the device is landscape)
+        switch sender.state {
+        case .began, .changed:
+            moveUpTheGrid(gesture: sender)
+        case .ended, .cancelled:
+            slideAndHideTheGrid(gesture: sender)
+        default:
+            break
+        }
+    }
+    /// moveUpTheGrid function allows the user to move up and down the grid
+    private func moveUpTheGrid(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: grid)
+        grid.transform = CGAffineTransform(translationX: 0, y: translation.y)
+    }
+    
+    ///slideAndHideTheGrid function move the grid out of the screen in the upper non visible area if the gesture is enought
+    private func slideAndHideTheGrid(gesture: UIPanGestureRecognizer) {
+        let screenHeight = UIScreen.main.bounds.height
+        let gridHeight = grid.bounds.height
+        let translation = gesture.translation(in: grid)
+        var translationTransform: CGAffineTransform
+        
+        if translation.y < -gridHeight/5 {
+            translationTransform = CGAffineTransform(translationX: 0, y: -screenHeight)
+        } else {
+            translationTransform = .identity
+        }
+        UIView.animate(withDuration: 0.4, animations: {self.grid.transform = translationTransform}, completion: nil)
+    }
+    
 
 }
