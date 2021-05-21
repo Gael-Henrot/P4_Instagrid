4b26c5962c635dc6a336872bfd8a27d88fdee02c
HENROT Gaël <gael.henrot@expleogroup.com>
2021-05-21 16:47:41 +0200

    Defense version

--- P4_Instagrid/Controller/ViewController.swift
+++ P4_Instagrid/Controller/ViewController.swift
@@ -4,22 +4,6 @@
 //
 //  Created by Gaël HENROT on 03/05/2021.
 //
-/*
- L'application permet de créer des petits montages carrés de 3 ou 4 photos.
- 1. L'utilisateur choisit une des trois dispositions
- 1.1 L'utilisateur appuie sur un des trois ou quatres boutons:
- a. La coche de l'ancienne sélection est cachée
- b. La coche de la nouvelle sélection est affichée.
- c. La grille centrale change pour correspondre au choix de l'utilisateur
- 2. L'utilisateur choisit 3 ou 4 photos
- 2.1 L'utilisateur appuie sur un plus
- 2.2 L'utilisateur choisit une photo
- 3. L'utilisateur partage sa création
- 3.1 L'utilisateur glisse la grille vers le haut ou la gauche
- 3.2 La vue de partage apparait
- 3.3 L'utilisateur choisit son moyen de partage
- 3.4 La grille revient à sa place d'origine
- */
 
 import UIKit
 
@@ -51,7 +35,7 @@ class ViewController: UIViewController  {
     @IBOutlet weak var downRightButton: UIButton?
     
     //    Declaration of the grid
-    @IBOutlet weak var grid: UIView!
+    @IBOutlet weak var grid: UIView?
     
     //    Declaration of the swipe label
     @IBOutlet weak var swipeLabel: UILabel?
@@ -65,14 +49,13 @@ class ViewController: UIViewController  {
         
         //        Initialization of the share gesture to moving the grid
         let shareGesture = UIPanGestureRecognizer(target: self, action: #selector(dragTheGrid(_:)))
-        grid.addGestureRecognizer(shareGesture)
-        
-        //        let shareGesture = UISwipeGestureRecognizer(target: self, action: #selector(dragTheGrid(_:)))
-        //                Think about changing it according to orientation
-        //                shareGesture.direction = .up
-        //        grid.addGestureRecognizer(shareGesture)
+        grid?.addGestureRecognizer(shareGesture)
         
     }
+    
+    //=======================================
+    //MARK: Orientation methods
+    //=======================================
     
     /// This method define the orientation of the device thanks to size classes (modify the isPortrait value)
     func defineOrientation() {
@@ -103,9 +86,7 @@ class ViewController: UIViewController  {
     /// This method change the pattern of the middle view by hiding or not the buttons in the grid.
     func changePattern(pattern: PatternType) {
         
-        //        upLeftButton?.isHidden = false
         upRightButton?.isHidden = false
-        //        downLeftButton?.isHidden = false
         downRightButton?.isHidden = false
         Pattern12Button?.isSelected = false
         Pattern21Button?.isSelected = false
@@ -197,13 +178,13 @@ class ViewController: UIViewController  {
     //MARK: Share the grid
     //=======================================
     
+    /// This method modify the oriententation and the swipeLabel each time the device is moved.
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         defineOrientation()
         defineSwipeLabel()
     }
     
     @objc func dragTheGrid(_ sender: UIPanGestureRecognizer) {
-        // think to add a condition (only use .up direction if the device is portrait and .left if the device is landscape)
         switch sender.state {
         case .began, .changed:
             moveTheGrid(sender)
@@ -217,9 +198,9 @@ class ViewController: UIViewController  {
     func moveTheGrid(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: grid)
         if isPortrait == true {
-            grid.transform = CGAffineTransform(translationX: 0, y: translation.y)
+            grid?.transform = CGAffineTransform(translationX: 0, y: translation.y)
         } else {
-            grid.transform = CGAffineTransform(translationX: translation.x, y: 0)
+            grid?.transform = CGAffineTransform(translationX: translation.x, y: 0)
         }
     }
     
@@ -235,12 +216,12 @@ class ViewController: UIViewController  {
         
         if isPortrait == true {
             screenSize = UIScreen.main.bounds.height
-            gridSize = grid.bounds.height
+            gridSize = grid?.bounds.height ?? 240
             translationDirection = translation.y
             translationTransform = CGAffineTransform(translationX: 0, y: -screenSize)
         } else {
             screenSize = UIScreen.main.bounds.width
-            gridSize = grid.bounds.width
+            gridSize = grid?.bounds.width ?? 240
             translationDirection = translation.x
             translationTransform = CGAffineTransform(translationX: -screenSize, y: 0)
         }
@@ -251,19 +232,23 @@ class ViewController: UIViewController  {
         } else {
             translationTransformation = .identity
         }
-        UIView.animate(withDuration: 0.4, animations: {self.grid.transform = translationTransformation}, completion: nil)
+        UIView.animate(withDuration: 0.4, animations: {self.grid?.transform = translationTransformation}, completion: nil)
     }
     
     /// This method returns the grid to its original position.
     func moveBackTheGrid() {
         let translationTransform: CGAffineTransform = .identity
-        UIView.animate(withDuration: 0.4, animations: {self.grid.transform = translationTransform}, completion: nil)
+        UIView.animate(withDuration: 0.4, animations: {self.grid?.transform = translationTransform}, completion: nil)
     }
     
     /// This method open an Activity Controller and present the possible actions to realize with the image of the grid.
     /// It call the moveBackTheGrid method when the user has done is choice.
     func shareTheGrid() {
-        let activityViewController = UIActivityViewController(activityItems: [convertTheGridToImage()], applicationActivities: nil)
+        guard let imageGrid = convertTheGridToImage() else {
+            moveBackTheGrid()
+            return
+        }
+        let activityViewController = UIActivityViewController(activityItems: [imageGrid], applicationActivities: nil)
         present(activityViewController, animated: true, completion: nil)
         
         activityViewController.completionWithItemsHandler = { (activityType: UIActivity.ActivityType?, completed:
@@ -272,12 +257,15 @@ class ViewController: UIViewController  {
         }
     }
     
-    /// This method convert the grid in to a UIImage and return it.
-    func convertTheGridToImage() -> UIImage {
-        let renderer = UIGraphicsImageRenderer(size: grid.bounds.size)
-        let image = renderer.image { ctx in
-            grid.drawHierarchy(in: grid.bounds, afterScreenUpdates: true)
+    /// This method convert the grid into a UIImage and return it.
+    func convertTheGridToImage() -> UIImage? {
+        guard let gridUnwrapped = grid else {
+            return nil
         }
+            let renderer = UIGraphicsImageRenderer(size: gridUnwrapped.bounds.size)
+            let image = renderer.image { ctx in
+            gridUnwrapped.drawHierarchy(in: gridUnwrapped.bounds, afterScreenUpdates: true)
+            }
         return image
     }
     
@@ -291,7 +279,7 @@ extension ViewController: UIImagePickerControllerDelegate {
         
         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
         currentButton?.setImage(image, for: .normal) // Replace the previous image (plus or previously choosed image) in the current plus button
-        currentButton?.imageView!.contentMode = .scaleAspectFill // Change the content mode of the current button to
+        currentButton?.imageView?.contentMode = .scaleAspectFill // Change the content mode of the current button to scaleAspectFill
         picker.dismiss(animated: true, completion: nil)
     }
     
